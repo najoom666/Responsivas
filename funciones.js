b4729cdd4fae89ec202cf00d0de414cb3b037871
nahomi <rosalesmartinez383@gmail.com>
2024-08-06 15:08:36 -0600

    Primer commit

---
+++ funciones.js
@@ -0,0 +1,7 @@
+function mostrar(){
+    document.getElementById('desplegable').classList.add('abrir');
+}
+
+function ocultar(){
+    document.getElementById('desplegable').classList.remove('abrir');
+}
